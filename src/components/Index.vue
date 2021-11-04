474405ab52f62155bd2d70d06453316a9473c2cc
andreichayeuski <andreichayeuski@gmail.com>
2021-11-04 20:06:34 +0300

    Initial commit

---
+++ src/components/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <SideBar :msg="message"/>
+  </div>
+</template>
+
+<script>
+import SideBar from './common/sideBar/SideBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SideBar
+  },
+  computed: {
+    message: function () {
+      return 'Andrei'
+    }
+  }
+}
+</script>
---
+++ src/components/common/sideBar/Tabs.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>
+      <dropdown-menu :menuTitle="menuTitle">
+        <section class="option">
+          <button @click="sayHello">This is button for method</button>
+          <span class="desc"
+            >This is Vue dropdown menu method that says hello for you.</span
+          >
+        </section>
+
+        <section class="option">
+          <a href="https://duckduckgo.com">This is basic a -link</a>
+          <span class="desc">Clicking this takes you somewhere else.</span>
+        </section>
+
+        <section class="option">
+          <router-link to="/about">This is Vue router link</router-link>
+          <span class="desc">Clicking this takes you somewhere else.</span>
+        </section>
+      </dropdown-menu>
+    </div>
+  </div>
+</template>
+
+<script>
+import dropdownMenu from '../utils/dropdown-menu.vue'
+export default {
+  components: { dropdownMenu },
+  props: ['menuTitle'], // Menu title from the parent
+  data () {
+    return {
+      isOpen: false // Variable if the menu is open or closed
+    }
+  },
+  methods: {
+    openClose () {
+      // Toggle between open or closed ( true || false )
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
