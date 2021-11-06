d5689f21a517697f9919d0f42a4acb5a294c0abe
andreichayeuski <andreichayeuski@gmail.com>
2021-11-06 22:09:09 +0300

    Dropdown hovered

--- src/components/common/sideBar/Tab.vue
+++ src/components/common/sideBar/Tab.vue
@@ -1,25 +1,30 @@
 <template>
-  <div>
-    <div class="tab" ref="menu" @click="openClose">
+  <div @mouseover="mouseover" @mouseleave="mouseleave">
+    <div :class="tabClass" ref="menu" @click="openClose">
       <span class="side-bar-icon">
         <Icons
           width="23"
           height="20"
           viewBox="0 0 16 20"
-          :icon-name="item.icon"
+          :icon-name="itemIcon"
         />
       </span>
       <span class="side-bar-text"> {{ item.label }} </span>
-      <Arrows class="side-bar-arrow" :arrow-name="arrowName" v-show="item.children.length > 0" />
+      <Arrows
+        class="side-bar-arrow"
+        :arrow-name="arrowName"
+        v-show="item.children.length > 0"
+      />
     </div>
-    <section
-      class="option"
-      v-for="(label, index) in item.children"
-      :key="index"
-      v-show="isOpen"
-    >
-      <a href="https://duckduckgo.com">{{ label }} </a>
-    </section>
+    <div class="tab-options" v-show="isOpen">
+      <li
+        class="tab-option"
+        v-for="(label, index) in item.children"
+        :key="index"
+      >
+        <span>{{ label }}</span>
+      </li>
+    </div>
   </div>
 </template>
 
@@ -35,12 +40,20 @@ export default {
   props: ["item"], // Menu title from the parent
   data() {
     return {
-      isOpen: false, // Variable if the menu is open or closed
+      isOpen: false, // Variable if the menu is open or closed,
+      isHovered: false,
+      itemIcon: "",
     };
   },
   computed: {
     arrowName: function () {
-      return this.isOpen ? "arrow-up-light" : "arrow-down-light";
+      return (this.isOpen ? "arrow-up-" : "arrow-down-") + this.arrowColor;
+    },
+    arrowColor: function () {
+      return this.isHovered ? "dark" : "light";
+    },
+    tabClass: function () {
+      return this.isHovered ? "tab hover" : "tab";
     },
   },
   methods: {
@@ -48,6 +61,19 @@ export default {
       // Toggle between open or closed ( true || false )
       this.isOpen = !this.isOpen;
     },
+    mouseover() {
+      this.itemIcon = this.itemIcon.toLowerCase().replace("default", "used");
+      this.isHovered = true;
+      this.isOpen = true;
+    },
+    mouseleave() {
+      this.itemIcon = this.item.icon;
+      this.isHovered = false;
+      this.isOpen = false;
+    },
+  },
+  mounted: function () {
+    this.itemIcon = this.item.icon;
   },
 };
 </script>
